a3cd50c31a69739a6f508564870cb4bf7cba29a2
Md. Nazrul Islam <nazrulnsu@gmail.com>
2022-09-08 09:31:08 +0600

    Partially worked on spring batch config

---
+++ src/main/java/io/javabrains/ipldashbaord/data/BatchConfig.java
@@ -0,0 +1,21 @@
+package io.javabrains.ipldashbaord.data;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+
+  @Autowired
+  public JobBuilderFactory jobBuilderFactory;
+
+  @Autowired
+  public StepBuilderFactory stepBuilderFactory;
+
+   
+
+}
