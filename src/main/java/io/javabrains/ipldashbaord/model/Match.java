e5f942e417954192d1e05ccf6b15b8c9933e923d
Md. Nazrul Islam <nazrul@nazrul.therapbd.net>
2022-09-11 13:21:31 +0600

    Added full spring batch config

--- src/main/java/io/javabrains/ipldashbaord/model/Match.java
+++ src/main/java/io/javabrains/ipldashbaord/model/Match.java
@@ -2,7 +2,13 @@ package io.javabrains.ipldashbaord.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Match {
+
+    @Id
     private long id;
     private String city;
     private LocalDate date;
