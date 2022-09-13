4d778d36123f4110f4701292d36ff213efed4155
Md. Nazrul Islam <nazrulnsu@gmail.com>
2022-09-13 17:14:26 +0600

    Added some JPA queries

--- src/main/java/io/javabrains/ipldashbaord/data/MatchDataProcessor.java
+++ src/main/java/io/javabrains/ipldashbaord/data/MatchDataProcessor.java
@@ -39,11 +39,12 @@ public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
   
     match.setTeam1(firstInningsTeam);
     match.setTeam2(secondInningsTeam);
-
+    match.setMatchWinner(matchInput.getWinner());
     match.setTossWinner(matchInput.getToss_winner());
     match.setTossDecision(matchInput.getToss_decision());
     match.setResult(matchInput.getResult());
     match.setResultMargin(matchInput.getResult_margin());
+    
     match.setUmpire1(matchInput.getUmpire1());
     match.setUmpire2(matchInput.getUmpire2());
 
