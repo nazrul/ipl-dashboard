ed9bfb01c6e81498b9a906601dfb605fac676350
Md. Nazrul Islam <nazrulnsu@gmail.com>
2022-09-14 17:51:43 +0600

    Pulled data through REST API in react app

--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,18 +1,26 @@
-import { React, useEffect } from 'react';
+import { React, useEffect, useState } from 'react';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 
 export const TeamPage = () => {
 
+  const [team, setTeam] = useState();   
+
   useEffect(
-    () =>{
-        
+    () => {
+        const fetchMatches = async () => {
+            const response = await fetch('http://localhost:8080/team/Delhi%20Capitals');
+            const data = await response.json();
+            // setTeam(data);
+            console.log(data);
+        };
+        fetchMatches();
     }
 
   );  
   return (
     <div className="TeamPage">
-      <h1>Team Name</h1>    
+      <h1>team.teamName</h1>    
       <MatchDetailCard/>
       <MatchSmallCard/>
       <MatchSmallCard/>
